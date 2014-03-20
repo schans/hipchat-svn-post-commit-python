f576a3315616ed4904eb20f1fbc24101a6a059b6
Bart van der Schans <schans@onehippo.com>
2014-03-20 11:36:38 +0100

    Add support for config file for setting token, room and name

--- hipchat-svn-post-commit.py
+++ hipchat-svn-post-commit.py
@@ -5,6 +5,10 @@
 # Scott Vitale
 # svvitale@gmail.com
 
+#
+# Set config values in code, command line, or config file
+#
+
 # This script is set to publish information after SVN commits to HipChat. 
 #
 # Required files/Application/services:
@@ -20,15 +24,19 @@ import getopt
 import urllib
 import urllib2
 import re
+import ConfigParser
 
 # Default room name
-room = "<room name>"
+ROOM = "<room name>"
 
 # Default token
-token = "<token>"
+TOKEN = "<token>"
 
 # Default chat user name
-user="Subversion"
+NAME="Subversion"
+
+# Default config
+CONFIG="/etc/svn/hipchat.cfg"
 
 # Default svnlook location
 svnlook="/usr/bin/svnlook"
@@ -74,14 +82,19 @@ def getCommitInfo( repo, revision ):
 	return chatMsg
 
 def main():
-        repository = False
         revision = False
+        repository = False
+        cmd_config = False
+        cmd_token = False
+        cmd_name = False
+        cmd_room = False
+
         try:
-                opts, args = getopt.getopt(sys.argv[1:], "r:s:k:t:u:", ["revision=", "repository=", "config=", "room=", "token=", "user="])
+                opts, args = getopt.getopt(sys.argv[1:], "r:s:f:k:t:u:", ["revision=", "repository=", "config=", "room=", "token=", "user="])
         except getopt.GetoptError, err:
                 print >>sys.stderr, str(err)
                 print "Usage:"
-                print "hipchat-svn-post-commit.py -r <revision> -s <repository> [-k room] [-t token] [-n name]"
+                print "hipchat-svn-post-commit.py -r <revision> -s <repository> [-f config] [-k room] [-t token] [-n name]"
                 sys.exit(1)
         for o, a in opts:
                 if o in ("-r", "--revision"):
@@ -89,11 +102,45 @@ def main():
                 elif o in ("-s", "--repository"):
                        repository = a
                 elif o in ("-k", "--room"):
-                       room = a
+                       cmd_room = a
                 elif o in ("-t", "--token"):
-                       token = a
+                       cmd_token = a
                 elif o in ("-n", "--name"):
-                       name = a
+                       cmd_name = a
+                elif o in ("-f", "--config"):
+                       cmd_config = a
+                       if not os.path.isfile(cmd_config):
+                                print "Config file does not exist:", cmd_config
+                                sys.exit(2)
+        # set defaults
+        token = TOKEN
+        name = NAME
+        room = ROOM
+
+        # config overrides defaultsif cmd_room:
+        if cmd_config:
+        	     config_file = cmd_config
+       	else:
+        	     config_file = CONFIG
+
+        if os.path.isfile(config_file):
+                config = ConfigParser.ConfigParser()
+                config.read(config_file)
+                if config.has_section('hipchat'):
+                        if config.has_option('hipchat', 'TOKEN'):
+                	            token = config.get('hipchat', 'TOKEN')
+                        if config.has_option('hipchat', 'NAME'):
+                	            name = config.get('hipchat', 'NAME')
+                        if config.has_option('hipchat', 'TOKEN'):
+                	            room = config.get('hipchat', 'ROOM')
+        
+        # cmd line overrides defaults and config
+        if cmd_token:
+        	     token = cmd_token
+        if cmd_name:
+        	     name = cmd_name
+        if cmd_room:
+        	     room = cmd_room
 
         if not revision:
                 print "Sepcify a revision with -r or --revision"
